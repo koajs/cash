12df6e363c886983d9c3f74dedf27caa8b657c56
Shaun Warman <shaunwarman1@gmail.com>
2020-07-04 12:44:56 -0500

    feat: add X-Cached-Response header behind option (#56)
    * feat: add X-Cached-Response header behind option
    
    * fix: fixed verbiage
    
    Co-authored-by: niftylettuce <niftylettuce@gmail.com>

--- test/when-cached.js
+++ test/when-cached.js
@@ -5,20 +5,19 @@ const test = require('ava');
 
 const cash = require('..');
 
-const createApp = function(c, opts) {
+const createApp = function(c, opts = {}) {
   const app = new Koa();
   app.use(
-    cash(
-      opts || {
-        get(key) {
-          return c.get(key);
-        },
-        set(key, value) {
-          return c.set(key, value);
-        },
-        compression: true
-      }
-    )
+    cash({
+      get(key) {
+        return c.get(key);
+      },
+      set(key, value) {
+        return c.set(key, value);
+      },
+      compression: true,
+      ...opts
+    })
   );
   return app;
 };
@@ -45,7 +44,7 @@ test.cb('when cached when the method is GET it should serve from cache', t => {
   const app = createApp(c);
   app.use(async function(ctx) {
     if (await ctx.cashed()) return;
-    throw new Error('wtf');
+    throw new Error('oops');
   });
 
   request(app.listen())
@@ -58,11 +57,31 @@ test.cb('when cached when the method is GET it should serve from cache', t => {
 });
 
 test.cb(
+  'when setCachedHeader is true, serve from cache should set appropriate header',
+  t => {
+    const app = createApp(c, { setCachedHeader: true });
+    app.use(async function(ctx) {
+      if (await ctx.cashed()) return;
+      throw new Error('oops');
+    });
+
+    request(app.listen())
+      .get('/')
+      .expect(200)
+      .expect('Content-Type', 'text/lol; charset=utf-8')
+      .expect('Content-Encoding', 'identity')
+      .expect('X-Cached-Response', 'HIT')
+      .expect('ETag', '"lol"')
+      .expect('lol', t.end);
+  }
+);
+
+test.cb(
   'when cached when the method is POST it should not serve from cache',
   t => {
     const app = createApp(c);
     app.use(async function(ctx) {
-      if (await ctx.cashed()) throw new Error('wtf');
+      if (await ctx.cashed()) throw new Error('oops');
       ctx.body = 'lol';
     });
 
@@ -76,7 +95,7 @@ test.cb('when cached when the response is fresh it should 304', t => {
   const app = createApp(c);
   app.use(async function(ctx) {
     if (await ctx.cashed()) return;
-    throw new Error('wtf');
+    throw new Error('oops');
   });
 
   request(app.listen())
